729fb09f08966a64d91d8a77d02a430effea6ece
Eric Roy <ericsroy@gmail.com>
2023-08-12 14:28:16 -0700

    whitespace

--- scripts/save.sh
+++ scripts/save.sh
@@ -28,7 +28,7 @@ mkdir -p "$LOCAL_PATH"
 trap cleanup EXIT
 
 echo "Creating compressed archive..."
-time tar -C "$LOCAL_PATH" --zstd -cf "$temp_file" "." 
+time tar -C "$LOCAL_PATH" --zstd -cf "$temp_file" "."
 
 echo "Uploading archive..."
 time aws s3 cp "$temp_file" "$remote_object" --no-progress
